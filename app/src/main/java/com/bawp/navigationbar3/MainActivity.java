0cdca32835cc2593acdb99be413be438ba070312
Andrea Blanco <56937994+Andr3aBlanco@users.noreply.github.com>
2023-02-07 20:30:38 -0800

    Initial commit Project Creation

---
+++ app/src/main/java/com/bawp/navigationbar3/MainActivity.java
@@ -0,0 +1,71 @@
+package com.bawp.navigationbar3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.bawp.navigationbar3.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //bind
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setBackground(null);
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.menu_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+                case R.id.menu_orders:
+                    replaceFragment(new OrdersFragment());
+
+                    break;
+
+                case R.id.menu_profile:
+                    replaceFragment(new ProfileFragment());
+
+                    break;
+
+                case R.id.menu_stats:
+                    replaceFragment(new StatsFragment());
+
+                    break;
+
+                case R.id.menu_add:
+                    replaceFragment(new HomeFragment());
+
+                    break;
+
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.barFrame, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
